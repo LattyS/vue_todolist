37ab99ef6f3941621a291e80a4a9d4f41a2361df
LattyS <lauries@outlook.fr>
2017-09-28 01:04:32 +0200

    data initialization and data binbing test without loop

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,14 @@
 {
     'use strict';
+
+    new Vue({
+        el: 'main#app',
+        data: {
+            tasks: [
+                {title: "Apprendre Vue.js", isDone: true},
+                {title: "Faire une To Do List", isDone: false},
+                {title: "Envoyer un mail au prof", isDone: false}
+            ]
+        }
+    });
 }
